ba5620b361f37d49aa1c36e5a45577ab85a0a8ce
Will Gordon <wgordon@redhat.com>
2023-05-29 20:57:42 -0400

    Catch up changes

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "cluster-subnets-string" {
   description = "Comma-separated string of all subnet IDs created for this cluster."
 }
 
+output "cluster-subnets-string" {
+  value       = join(",", concat(module.vpc.public_subnets, module.vpc.private_subnets))
+  description = "Comma-separated string of all subnet IDs created for this cluster."
+}
+
 output "cluster-infra-id" {
   value = resource.random_string.infra_id.result
   description = "Infra ID randomly generated for the cluster"
